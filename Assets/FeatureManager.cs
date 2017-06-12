41d095eec29627459514a3ecddd88793c72a8bae
aries <906130960@qq.com>
2017-06-13 06:42:43 +0800

    A Character Edit Demo
    A character edit demo.(Unity Ver 5.6.1)

---
+++ Assets/FeatureManager.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class FeatureManager : MonoBehaviour {
+
+    public List<Feature> features;
+    public int currFeature;
+
+    void OnEnable(){
+        LoadFeatures();
+    }
+
+    void OnDisable()
+    {
+        SaveFeatures();
+    }
+
+      void LoadFeatures()
+    {
+        features = new List<Feature>();
+        features.Add(new Feature("Face",transform.Find("Face").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Hair", transform.Find("Face").transform.Find("Hair").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Eyes", transform.Find("Face").transform.Find("Eyes").GetComponent<SpriteRenderer>()));
+        features.Add(new Feature("Mouth", transform.Find("Face").transform.Find("Mouth").GetComponent<SpriteRenderer>()));
+    }
+
+    void SaveFeatures()
+    {
+
+    }
+
+
+    public void SetCurrent(int index)
+    {
+        if (features == null)
+            return;
+
+        currFeature = index;
+    }
+
+    public void NextChoice()
+    {
+        if (features == null)
+            return;
+     
+        features[currFeature].currIndex++;
+
+        features[currFeature].UpdateFeature();
+    }
+
+    public void PreviousChoice()
+    {
+        if (features == null)       
+            return;
+
+        features[currFeature].currIndex--;
+
+        features[currFeature].UpdateFeature();
+    }
+
+
+}
+
+[System.Serializable]
+public class Feature
+{
+    public string ID;                    //部件名称
+    public int currIndex;                //当前部件选项编号
+    public Sprite[] choices;             //部件选项数组
+    public SpriteRenderer renderer;      //精灵渲染器
+
+    public Feature(string id,SpriteRenderer rend)
+    {
+        ID = id;
+        renderer = rend;
+        UpdateFeature();
+    }
+
+    public void UpdateFeature()
+    {
+        choices = Resources.LoadAll<Sprite>("lapuras/" + ID);
+
+        if (choices == null || renderer == null)
+            return;
+
+        //到数组循环
+        if (currIndex < 0)
+            currIndex = choices.Length - 1;
+        if (currIndex >= choices.Length)
+            currIndex = 0;
+
+        renderer.sprite = choices[currIndex];
+
+        Debug.Log("cww"+currIndex);
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class UIManager : MonoBehaviour {
+
+    public RectTransform btnPrefab;
+
+    private FeatureManager mgr;
+    private Text descText;
+    private List<Button> buttons;
+
+	// Use this for initialization
+	void Start () {
+        mgr = FindObjectOfType<FeatureManager>();
+        descText = transform.Find("Navigation").Find("Text").GetComponent<Text>();
+        transform.Find("Navigation").Find("Previous").GetComponent<Button>().onClick.AddListener(() => mgr.PreviousChoice());
+        transform.Find("Navigation").Find("Next").GetComponent<Button>().onClick.AddListener(() => mgr.NextChoice());
+        InitialzeFeatureButtons(); 
+    }
+    
+    void InitialzeFeatureButtons()
+    {
+        buttons = new List<Button>();
+        float height = btnPrefab.rect.height;
+        float width = btnPrefab.rect.width;
+        for(int i = 0; i < mgr.features.Count; i++)
+        {
+            RectTransform temp = Instantiate<RectTransform>(btnPrefab);
+            temp.name = i.ToString();
+            temp.SetParent(transform.Find("Features").GetComponent<RectTransform>());
+            temp.localScale = new Vector3(1, 1, 1);
+            temp.localPosition = new Vector3(0, 0, 0);
+            temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, width);
+            temp.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, i * height, height);
+
+            Button b = temp.GetComponent<Button>();
+            b.onClick.AddListener(() => mgr.SetCurrent(int.Parse(temp.name)));
+            buttons.Add(b);
+        }
+    }
+
+    void UpdateFeatureButtons()
+    {
+        for(int i = 0; i < mgr.features.Count; i++)
+        {
+            buttons[i].transform.Find("FeatureImage").GetComponent<Image>().sprite = mgr.features[i].renderer.sprite;
+        }
+    }	
+
+
+	// Update is called once per frame
+	void Update () {
+        UpdateFeatureButtons();
+        descText.text = mgr.features[mgr.currFeature].ID +"#"+(mgr.features[mgr.currFeature].currIndex+1).ToString();
+	}
+}
